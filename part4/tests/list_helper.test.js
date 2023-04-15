600d4e1bb8f527b0a0915081cf607d5bc70d4aed
Cadence <77511497+CadenceElaina@users.noreply.github.com>
2023-04-15 19:26:33 -0400

    4.6* w/o Lodash because I can ^-^

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -71,7 +71,64 @@ const blogs = [
     __v: 0
   }
 ]
-
+const multipleAuthorsWithMaxBlogs = [
+  {
+    _id: "5a422a851b54a676234d17f1",
+    title: "Was it worth it?",
+    author: "Cadence",
+    url: "https://verbosecode.com/",
+    likes: 42.0,
+    __v: 0
+  },
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Edsger W. Dijkstra",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0
+  },
+  {
+    _id: "5a422b891b54a676234d17fa",
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
+    __v: 0
+  },
+  {
+    _id: "5a422ba71b54a676234d17fb",
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 0,
+    __v: 0
+  },
+  {
+    _id: "5a422bc61b54a676234d17fc",
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2,
+    __v: 0
+  }
+]
 
 describe('total likes', () => {
   test('of empty list is zero', () => {
@@ -117,6 +174,36 @@ describe('favoriteBlog', () => {
       url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
       likes: 12,
       __v: 0
+    })
+  })
+})
+
+describe('author(s) with most blogs', () => {
+  test('of empty list is zero', () => {
+    const result = listHelper.authorWithMostBlogs(zeroBlogs)
+    expect(result).toEqual(null)
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.authorWithMostBlogs(listWithOneBlog)
+    expect(result).toEqual({
+      author: ['Edsger W. Dijkstra'],
+      blogs: 1
+    })
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.authorWithMostBlogs(blogs)
+    expect(result).toEqual({
+      author: ['Robert C. Martin'],
+      blogs: 3
+    })
+  })
+  test('the max number of blogs authored is the same among multiple authors', () => {
+    const result = listHelper.authorWithMostBlogs(multipleAuthorsWithMaxBlogs)
+    expect(result).toEqual({
+      authors: ['Edsger W. Dijkstra', 'Robert C. Martin'],
+      blogs: 3
     })
   })
 })  
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -27,7 +27,62 @@ const favoriteBlog = (blogs) => {
   let mostLikes = blogs[maxIndex]
   return mostLikes
 }
+const authorWithMostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+  let authors = []
 
+  blogs.map((blog, i) => {
+    /*Prevents duplicates of author names and .some stops as soon as we determine the array already has author name */
+    if (!authors.some(a => a.author === blog.author)) {
+      authors = [...authors, { author: blog.author, blogs: 0 }]
+    }
+  })
+  //increments blog count for authors
+  blogs.map((blog, i) => {
+    authors.find((author, i) => {
+      if (author.author === blog.author) {
+        authors[i].blogs++
+      }
+    })
+
+  })
+  // console.log(authors)
+
+  //Alternative way to calculate max, but does not consider multiple authors with same number of blogs
+  /*  const max = blogs.reduce((prev, current) => (prev.blogs > current.blogs) ? prev : current).author */
+  /* const numOfBlogs = authors.find(author => author.author === max).blogs */
+  //console.log(numOfBlogs)
+
+  const max1 = Math.max(...authors.map(o => o.blogs))
+  //console.log(max1)
+  let allMax = authors.filter(author => author.blogs === max1)
+
+  let names = allMax.map(author => author.author)
+  //console.log(names)
+  let authorsWithMostBlogs
+  let authorWithMostBlogs
+  //Note the below changes what the tests should check for
+  // i.e authors vs author as key name in isEqualTo
+  if (names.length > 1) {
+    authorsWithMostBlogs = {
+      authors: names,
+      blogs: max1
+    }
+    return authorsWithMostBlogs
+  }
+  else {
+    authorWithMostBlogs = {
+      author: names,
+      blogs: max1
+    }
+    return authorWithMostBlogs
+  }
+
+  // console.log(author)
+  // return authors
+}
 module.exports = {
-  totalLikes, favoriteBlog
+  totalLikes, favoriteBlog, authorWithMostBlogs
 }
