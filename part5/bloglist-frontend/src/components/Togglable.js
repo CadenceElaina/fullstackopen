cb34ee0176c636f1cf42857d4f083e230733b436
Cadence <77511497+CadenceElaina@users.noreply.github.com>
2023-04-24 17:18:20 -0400

    part 5.12 - define PropTypes & add ESlint

--- part5/bloglist-frontend/src/components/Togglable.js
+++ part5/bloglist-frontend/src/components/Togglable.js
@@ -1,4 +1,5 @@
-import { useState, forwardRef, useImperativeHandle } from 'react'
+import { useState, forwardRef, useImperativeHandle } from 'react';
+import PropTypes from 'prop-types';
 
 const Togglable = forwardRef((props, refs) => {
   const [visible, setVisible] = useState(false);
@@ -28,5 +29,11 @@ const Togglable = forwardRef((props, refs) => {
     </div>
   )
 });
+
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired
+};
+
+Togglable.displayName = 'Togglable'
 
 export default Togglable;
