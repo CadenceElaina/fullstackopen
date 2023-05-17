faf775d5d4ae07ca855613be525f1c093c5cfffb
Cadence <77511497+CadenceElaina@users.noreply.github.com>
2023-05-16 20:44:49 -0400

    8.7

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -126,6 +126,10 @@ const typeDefs = `
       published: Int!
       genres: [String!]!
     ) : Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 
 `;
@@ -168,6 +172,16 @@ const resolvers = {
       const book = { ...args, id: uuid() };
       books = books.concat(book);
       return book;
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((a) => a.name === args.name);
+      if (!author) {
+        return null;
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo };
+      authors = authors.map((a) => (a.name === args.name ? updatedAuthor : a));
+      return updatedAuthor;
     },
   },
 };
