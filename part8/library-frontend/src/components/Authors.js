68a35ec1c41d0eac8c5f600c9c7511b6ca15cc01
Cadence <77511497+CadenceElaina@users.noreply.github.com>
2023-05-18 21:14:36 -0400

    8.11 - 8.12

--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+import { useMutation } from "@apollo/client";
+
+import { ALL_AUTHORS, EDIT_AUTHOR } from "../queries";
+
 const Authors = ({ authors }) => {
-  if (!authors) {
+  const [born, setBorn] = useState("");
+  const [name, setName] = useState("");
+  /*   if (!authors) {
     return null;
-  }
+  } */
+
+  const [editAuthor, result] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  });
+
+  const submit = (event) => {
+    event.preventDefault();
+    /* console.log(typeof born);
+    console.log(typeof parseInt(born)); */
+    editAuthor({ variables: { name, setBornTo: parseInt(born) } });
+    setName("");
+    setBorn("");
+  };
+
+  useEffect(() => {
+    if (result.data && result.data.editAuthor === null) {
+      console.log("author not found");
+    }
+  }, [result.data]);
 
   return (
     <div className="container">
@@ -22,6 +48,26 @@ const Authors = ({ authors }) => {
           ))}
         </tbody>
       </table>
+      <h3>Set birthyear for an author</h3>
+      <form onSubmit={submit}>
+        <label htmlFor="author-select">Choose an Author:</label>
+        <select onChange={({ target }) => setName(target.value)}>
+          <option defaultValue="selected">select author</option>
+          {authors.map((a) => (
+            <option value={`${a.name}`} key={a.name}>
+              {a.name}
+            </option>
+          ))}
+        </select>
+        <div>
+          born
+          <input
+            value={born}
+            onChange={({ target }) => setBorn(target.value)}
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
     </div>
   );
 };
