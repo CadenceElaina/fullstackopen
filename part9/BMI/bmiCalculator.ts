8b3ee1a3732e5151e4228b92471e11a15474251a
Cadence <77511497+CadenceElaina@users.noreply.github.com>
2023-06-02 09:32:40 -0400

    9.3 Command Line - add error handling - and readline-sync to prompt user in command line

--- part9/BMI/bmiCalculator.ts
+++ part9/BMI/bmiCalculator.ts
@@ -1,3 +1,22 @@
+interface Values {
+  value1: number;
+  value2: number;
+}
+
+const parseArgs = (args: string[]): Values => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1: Number(args[2]),
+      value2: Number(args[3]),
+    };
+  } else {
+    throw new Error("Provided values were not numbers!");
+  }
+};
+
 const calculateBmi = (n1: number, n2: number): string => {
   let meters = n1 / 10;
   let squareOfHeight = meters * meters;
@@ -25,7 +44,8 @@ const calculateBmi = (n1: number, n2: number): string => {
 };
 
 try {
-  console.log(calculateBmi(180, 74));
+  const { value1, value2 } = parseArgs(process.argv);
+  console.log(calculateBmi(value1, value2));
 } catch (error: unknown) {
   let errorMessage = "Something went wrong: ";
   if (error instanceof Error) {
