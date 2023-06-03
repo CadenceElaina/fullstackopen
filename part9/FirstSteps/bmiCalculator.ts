28718a528baac2c091a4753b5d69a882f893f78b
Cadence <77511497+CadenceElaina@users.noreply.github.com>
2023-06-02 21:16:12 -0400

    9.5 WebBMI

--- part9/FirstSteps/bmiCalculator.ts
+++ part9/FirstSteps/bmiCalculator.ts
@@ -1,8 +1,8 @@
-interface Values {
+/* interface Values {
   value1: number;
   value2: number;
-}
-
+} */
+/* 
 const parseArgs = (args: string[]): Values => {
   if (args.length < 4) throw new Error("Not enough arguments");
   if (args.length > 4) throw new Error("Too many arguments");
@@ -15,35 +15,39 @@ const parseArgs = (args: string[]): Values => {
   } else {
     throw new Error("Provided values were not numbers!");
   }
-};
+}; */
 
-const calculateBmi = (n1: number, n2: number): string => {
-  let meters = n1 / 10;
+export const calculateBmi = (height: number, weight: number): string => {
+  /*   console.log(height, weight);
+  let meters = height / 10;
   let squareOfHeight = meters * meters;
-  let result = Number(((n2 / squareOfHeight) * 100).toFixed(2));
+  let result = Number(((weight / squareOfHeight) * 100).toFixed(2)); */
 
-  if (result < 16) {
+  const bmi = weight / (height / 100) ** 2;
+
+  if (bmi < 16) {
     return "Underweight (Severe thinness)";
-  } else if (result > 16 && result <= 16.9) {
+  } else if (bmi > 16 && bmi <= 16.9) {
     return "Underweight (Moderate thinness)";
-  } else if (result >= 17 && result <= 18.4) {
+  } else if (bmi >= 17 && bmi <= 18.4) {
     return "Underweight (Mild thinness)";
-  } else if (result >= 18.5 && result <= 24.9) {
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
     return "Normal range";
-  } else if (result >= 25 && result <= 29.9) {
+  } else if (bmi >= 25 && bmi <= 29.9) {
     return "Overweight (Pre-obese)";
-  } else if (result >= 30 && result <= 34.9) {
+  } else if (bmi >= 30 && bmi <= 34.9) {
     return "Obese (Class I)";
-  } else if (result >= 35 && result <= 39.9) {
+  } else if (bmi >= 35 && bmi <= 39.9) {
     return "Obese (Class II)";
-  } else if (result >= 40) {
+  } else if (bmi >= 40) {
     return "Obese (Class III)";
-  } else {
+  } /* else {
     throw new Error("Must enter numbers above 0!");
-  }
+  } */
+  return "gurrl";
 };
 
-try {
+/* try {
   const { value1, value2 } = parseArgs(process.argv);
   console.log(calculateBmi(value1, value2));
 } catch (error: unknown) {
@@ -53,3 +57,4 @@ try {
   }
   console.log(errorMessage);
 }
+ */
