cda77dab1a0446df55268d4d47ac2c767a9319c2
Cadence <77511497+CadenceElaina@users.noreply.github.com>
2023-06-05 09:34:50 -0400

    9.10 endpoint /api/diagnoses shows all diagnoses

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import diagnosisRouter from "./routes/diagnoses";
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -10,6 +11,8 @@ app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
   res.send("pong");
 });
+
+app.use("/api/diagnoses", diagnosisRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
