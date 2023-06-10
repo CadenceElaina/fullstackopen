d19f3c71d00d8382d7c83c0cc1a8813a7a904f2f
Cadence <77511497+CadenceElaina@users.noreply.github.com>
2023-06-10 09:29:25 -0400

    remove part 9 in hopes that git cooperates

--- part9/courseinfo/src/components/Header.tsx
+++
@@ -1,5 +0,0 @@
-const Header = ({ courseName }: { courseName: string }) => {
-  return <h1>{courseName}</h1>;
-};
-
-export default Header;
--- part9/courseinfo/src/components/Total.tsx
+++
@@ -1,8 +0,0 @@
-import { CoursePart } from "../types";
-
-const Total = ({ parts }: { parts: CoursePart[] }) => {
-  const total = parts.reduce((carry, part) => carry + part.exerciseCount, 0);
-  return <p>Number of exercises {total}</p>;
-};
-
-export default Total;
